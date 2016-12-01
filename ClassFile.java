95643e07fa0bfe6dc49f49a5d6ed94f8871164c6
Matt Treichel <trei5470@mylaurier.ca>
2016-11-30 21:46:59 -0500

    Final Additions. Better Commit Message: - test1.txt, test2.txt, and test2-2.txt work - while loops and if/else structures work, but are definitely fragile and likely can't be nested - No handling for doubles, but could possible be as easy as some branch where TYPE.INT is TYPE.DOUBLE

--- ClassFile.java
+++ ClassFile.java
@@ -8,6 +8,7 @@ public class ClassFile implements Constants {
   private InstructionFactory _factory;
   private ConstantPoolGen    _cp;
   private ClassGen           _cg;
+  private boolean ifSymbolFlag = false;
   private boolean go_toFlag = false;
   private boolean bodyFlag = false;
   private boolean ifFlag = false;
@@ -63,6 +64,7 @@ public class ClassFile implements Constants {
 		process(statements.getChild(i), il);
 	}
 
+    // Cleanup
     il.append(_factory.createReturn(Type.VOID));
     method.setMaxStack();
     method.setMaxLocals();
@@ -75,17 +77,21 @@ public class ClassFile implements Constants {
   
   private void process(SyntaxTreeNode statement, InstructionList il) {
 
+	//This helps catch branching backtracking for 'if' structures 
+	//(it has to loop around a 2nd time to process the next instruction to fill in the goto)
 	if (ifFlag) {
 		ifFlag2 = true;
 	}
 	  
 	if (statement.toString().equals("=")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
+		
 		// Process Right Side
 		process(interior.getChild(1), il);
+		
 		// Process Left Side
 		if (!symbolTable.containsKey(interior.getChild(0).toString())) {
-			symbolTable.put(interior.getChild(0).toString(), registers);
+			symbolTable.put(interior.getChild(0).toString(), registers); // Note: ad-hoc symbolTable used to just map {varName:register#}
 			il.append(_factory.createStore(Type.INT, registers));
 			registers += 1;
 		} else {
@@ -97,16 +103,19 @@ public class ClassFile implements Constants {
 	else if (statement.toString().equals("while")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		SyntaxTreeNode.Interior body = (SyntaxTreeNode.Interior) interior.getChild(1);
+		
+		// Setup goto to be backtracked later
 	    BranchInstruction go_to = _factory.createBranchInstruction(Constants.GOTO, null);
 	    il.append(go_to);
 	    
-	    bodyFlag = true; // For grabbing backtrack target
+	    bodyFlag = true; // For grabbing backtrack target used in conditional
 		for (int i = 0; i < body.numChildren(); i++) {
 			process(body.getChild(i), il); // Body
 		}	
-		go_toFlag = true; // For grabbing goto target
+		go_toFlag = true; // For grabbing backtrack target
 	    process(interior.getChild(0), il); // Condition
 	    
+	    // Set backtrack target, the other is set within the conditional
 	    go_to.setTarget(go_toTarget);
 	}
 	
@@ -114,34 +123,40 @@ public class ClassFile implements Constants {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		SyntaxTreeNode.Interior body1 = (SyntaxTreeNode.Interior) interior.getChild(1);
 		SyntaxTreeNode.Interior body2 = null;
+		// Only process this part if 'else' part exists
 		if (interior.numChildren() == 3) {
 			body2 = (SyntaxTreeNode.Interior) interior.getChild(2);
 		}
 	    
+		ifSymbolFlag = true; // Symbols put into byte-code are opposite for 'if' branching
 	    process(interior.getChild(0), il); // Condition
+	    ifSymbolFlag = false;
 	    
 		for (int i = 0; i < body1.numChildren(); i++) {
 			process(body1.getChild(i), il); // Body
 		}
 		
+		// Setup goto to be backtracked later
 		BranchInstructions.add(_factory.createBranchInstruction(Constants.GOTO, null));
 	    il.append(BranchInstructions.get(BranchInstructions.size() - 1));
 	    
+	    // Process 'else' if necessary
 		if (body2 != null) {
-			elseFlag = true;
+			elseFlag = true; // For grabbing backtrack target used in else
 			for (int i = 0; i < body2.numChildren(); i++) {
 				process(body2.getChild(i), il); // Body Else
 			}	
 		}
 
-	    ifFlag = true;
+	    ifFlag = true; //Since this flag comes last without recursing deeper, ifFlag2 is needed on the next loop around to catch the next Instruction
 	}
 	
 	else if (statement.toString().equals("<")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
-		BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPLT, bodyTarget));
+		if (!ifSymbolFlag) BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPLT, bodyTarget));
+		else BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPGE, bodyTarget));
 		il.append(BranchInstructions.get(BranchInstructions.size() - 1));
 	}
 	
@@ -149,7 +164,8 @@ public class ClassFile implements Constants {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
-		BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPGT, bodyTarget));
+		if (!ifSymbolFlag) BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPGT, bodyTarget));
+		else BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPLE, bodyTarget));
 		il.append(BranchInstructions.get(BranchInstructions.size() - 1));
 	}
 	
@@ -157,16 +173,17 @@ public class ClassFile implements Constants {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
-		BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPLE, bodyTarget));
+		if (!ifSymbolFlag) BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPLE, bodyTarget));
+		else BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPGT, bodyTarget));
 		il.append(BranchInstructions.get(BranchInstructions.size() - 1));
-		
 	}
 	
 	else if (statement.toString().equals(">=")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
-		BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPGE, bodyTarget));
+		if (!ifSymbolFlag) BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPGE, bodyTarget));
+		else BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPLT, bodyTarget));
 		il.append(BranchInstructions.get(BranchInstructions.size() - 1));
 	}	
 	
@@ -174,7 +191,8 @@ public class ClassFile implements Constants {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
-		BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPEQ, bodyTarget));
+		if (!ifSymbolFlag) BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPEQ, bodyTarget));
+		else BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPNE, bodyTarget));
 		il.append(BranchInstructions.get(BranchInstructions.size() - 1));
 	}
 
@@ -182,21 +200,22 @@ public class ClassFile implements Constants {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
 		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
-		BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPNE, bodyTarget));
+		if (!ifSymbolFlag) BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPNE, bodyTarget));
+		else BranchInstructions.add(_factory.createBranchInstruction(Constants.IF_ICMPEQ, bodyTarget));
 		il.append(BranchInstructions.get(BranchInstructions.size() - 1));
 	}
 	
 	else if (statement.toString().equals("+")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
-		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
+		process(interior.getChild(0), il);
 		il.append(InstructionConstants.IADD);
 	}
 	
 	else if (statement.toString().equals("-")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
-		process(interior.getChild(0), il);
 		process(interior.getChild(1), il);
+		process(interior.getChild(0), il);
 		il.append(InstructionConstants.ISUB);
 	}
 	
@@ -223,9 +242,9 @@ public class ClassFile implements Constants {
 	
 	else if (statement.toString().equals("print")) {
 		SyntaxTreeNode.Interior interior = (SyntaxTreeNode.Interior) statement;
-		int var = (int)symbolTable.get(interior.getChild(0).toString());
+		int var = (int)symbolTable.get(interior.getChild(0).toString()); // Print grabs the variable's register number from symbolTable to pull from stack
 		il.append(_factory.createFieldAccess("java.lang.System", "out", new ObjectType("java.io.PrintStream"), Constants.GETSTATIC));
-		printTarget = il.getEnd();
+		printTarget = il.getEnd(); // Need to grab backtrack target here at start of print Instruction
 		il.append(_factory.createLoad(Type.INT, var));
 	    il.append(_factory.createInvoke("java.io.PrintStream", "println", Type.VOID, new Type[] { Type.INT }, Constants.INVOKEVIRTUAL));
 	}
@@ -240,7 +259,8 @@ public class ClassFile implements Constants {
 		}
 	}
 	
-	
+	// Backpatching
+	// The first two (for 'while') are kind of inconsistently done than the later two (for 'if')
     if (bodyFlag)  { // for while backpatching
     	bodyTarget = il.getEnd(); // ie. the first body instruction
     	bodyFlag = false;
@@ -252,7 +272,7 @@ public class ClassFile implements Constants {
 	if (ifFlag && ifFlag2) { // for if backpatching
 		if (printTarget == null) {
 			BranchInstructions.get(BranchInstructions.size() - 1).setTarget(il.getEnd());
-		} else {
+		} else { // Special case for 'print' instruction, since 3 Instructions instead of 1
 			BranchInstructions.get(BranchInstructions.size() - 1).setTarget(printTarget);
 		}
 	    ifFlag = false;
@@ -265,10 +285,7 @@ public class ClassFile implements Constants {
 	printTarget = null;
   }
   
-  public void ilappend(InstructionList il, InstructionHandle ih) {
-	  
-  }
-  
+  // Returns the leaf node value, ie. Number or Variable Name
 	public Object resolve(SyntaxTreeNode node) {
 		Object value;
 		if (node.getValue() instanceof SymbolTableEntry) {
